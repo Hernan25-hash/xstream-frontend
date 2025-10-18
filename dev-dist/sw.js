a3e36861eed984bcba7e879780f19d8af0af5c9e
Your Name <your-email@example.com>
2025-10-18 22:01:38 +0800

    Update site with new build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3rh7a3rh378"
+    "revision": "0.g0pd5n3hagg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
