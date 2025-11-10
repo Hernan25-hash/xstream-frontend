0ccb72b28c086aa9ba9b280a04bc5c39e8eca711
Your Name <your-email@example.com>
2025-11-10 16:00:05 +0800

    Add robots.txt for SEO and crawling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m07cdeqc2e"
+    "revision": "0.vrfj75jn9eo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
