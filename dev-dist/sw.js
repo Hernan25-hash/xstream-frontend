691f25fe65f14b63dd954e280c6b26ad8ca1a765
Your Name <your-email@example.com>
2025-11-27 17:10:16 +0800

    Update sitemap.xml and build files

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8pitpqiroi8"
+    "revision": "0.ppt6tq89df"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
